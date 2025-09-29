703dd24327f89c66c4eb801413042dd4b763e8a7
Nguyen Minh Quan <nmquan2108@gmail.com>
2025-09-30 00:53:04 +0700

    sửa lại icon và layout main activity

--- StockDashboard/app/build.gradle.kts
+++ StockDashboard/app/build.gradle.kts
@@ -34,12 +34,8 @@ android {
     }
 }
 
-dependencies {// build.gradle (Module)
-
-
-
+dependencies {
     implementation(libs.appcompat)
-    implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.annotation)
@@ -49,7 +45,6 @@ dependencies {// build.gradle (Module)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation("androidx.fragment:fragment:1.6.1")
-    implementation("com.google.android.material:material:1.4.0 ")
     implementation("com.google.android.flexbox:flexbox:3.0.0")
-
+    implementation("com.google.android.material:material:1.12.0")
 }
