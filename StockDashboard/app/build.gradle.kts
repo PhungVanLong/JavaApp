7caa9b4c5c522ad22005e6d4c595550ead04d2d7
HoangMinh <Minh40009@gmail.com>
2025-10-02 11:05:15 +0700

    update chart

--- StockDashboard/app/build.gradle.kts
+++ StockDashboard/app/build.gradle.kts
@@ -32,6 +32,7 @@ android {
     buildFeatures {
         viewBinding = true
     }
+    buildToolsVersion = "36.0.0"
 }
 
 dependencies {
@@ -48,4 +49,7 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.fragment:fragment:1.6.1")
+    implementation("com.github.AnyChart:AnyChart-Android:1.1.2")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.google.code.gson:gson:2.10.1")
 }
